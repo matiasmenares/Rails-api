fa205ee7e6d3e30ade9d32c0125319a091f09a0d
matiasmenares <dejaaymts@gmail.com>
2016-10-03 11:18:22 -0300

    Update and destroy

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::UsersController < ApplicationController
+	before_action :set_user,only: [:update,:destroy,:show]
 	respond_to :json
 	skip_before_filter :verify_authenticity_token
 
 	def show
-		respond_with User.find params[:id]
+		respond_with @user
 	end
 	def create
 		user = User.new(user_params)
@@ -13,8 +14,24 @@ class Api::V1::UsersController < ApplicationController
 			render json: { errors: user.errors}, status: 422
 		end
 	end
+	def update
+		if @user.update(user_params)
+		  render json: @user, status: 200
+		else
+		  render json: { errors: @user.errors }, status: 422
+		end
+	end
+
+	def destroy
+		@user.destroy
+		head 204
+	end
+
 
 	private
+	def set_user
+		@user = User.find(params[:id])
+	end
 	def user_params
 		params.require(:user).permit(:email, :password, :password_confirmation)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	namespace :api,defaults: {format: :json} do
 		namespace :v1 do
 			#get 'users/:id',to: "users#show"
-			resources :users, :only => [:show, :create]
+			resources :users, :only => [:show, :create,:update,:destroy]
 		end
 	end
 end
